ac1c5dbc52d6ff3b4481c63ef4f65122067fe136
FI-Nabil <63797144+FI-Nabil@users.noreply.github.com>
2023-04-12 20:54:36 +0600

    Update Segment Tree for the Sum.cpp

--- Segment Tree for the Sum.cpp
+++ Segment Tree for the Sum.cpp
@@ -4,51 +4,86 @@
 using namespace std;
 using namespace __gnu_pbds;
 #define ll long long
-#define ff first
-#define ss second
-#define pb push_back
-#define YES cout <<"Yes\n";
-#define NO cout <<"No\n";
-typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-const ll mxN = 1e7+5;
-ll spf[mxN];
+typedef tree<ll, null_type, greater_equal<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ll a[400000];
+ll t[400000];
 
-void pf(){
- for (ll i=1; i<mxN; i++) spf[i] = i;
- for (ll i=2; i<mxN; i++){
-  if(spf[i]==i){
-    for (ll j=i+i; j<mxN; j+=i){
-      spf[j] = min(spf[j], i);
-    }
+
+void build (ll node, ll b, ll e){
+     
+     if(b==e){
+      t[node] = a[b];
+      return;
+     }
+     ll left_node = 2*node;
+     ll right_node = 2*node + 1;
+     ll mid = (b+e)/2;
+     build(left_node, b, mid);
+     build (right_node, mid+1, e);
+     t[node] = t[left_node] + t[right_node];
+
+
+}
+ll query(ll node, ll b, ll e, ll i , ll j){
+  if(e<i or b>j) return 0;
+  if(b>=i and e<=j){
+    return t[node];
   }
- }
+  ll mid = (b+e)/2;
+  ll left_node = 2*node;
+  ll right_node = left_node + 1;
+  return query(left_node , b , mid , i, j ) + query (right_node , mid+1,e, i, j);
 }
 
-void dracarys(){
- ll x,y; cin >> x >> y;
- if(y-x == 1) {
-  cout << -1 <<'\n'; return;
- }
- ll gap = y-x;
- vector<ll>d;
- while(gap>1){
-   ll xd = spf[gap];
-   while(gap%xd==0) gap /= xd;
-   d.pb(xd);
- }
- ll ans = 1e16;
- for (auto p:d){
-    ll rc =p* ((x+p-1)/p);
-    ans = min(ans, rc-x);
- }
- cout << ans <<'\n';
+void update (ll node, ll b, ll e, ll i, ll x){
+      if(i>e or i<b)
+        return;
+      if(b==e and b==i){
+        t[node]=x;
+       return;
+      }
+
+      ll mid = (b+e)/2;
+      ll left_node = 2*node;
+      ll right_node = left_node + 1;
+      update (left_node, b , mid, i, x);
+      update (right_node, mid+1, e, i, x);
+      t[node] = t[left_node] + t[right_node];
 }
-int main(){
-    ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  pf();
-    ll tc=1; cin >> tc;
-    for (ll i=1; i<=tc; i++){
-        dracarys();
+
+
+void solve() {
+ ll n, q; cin >> n >> q;
+ for (ll i=1; i<=n; i++)
+   cin >> a[i];
+   build(1, 1, n);
+   while(q--){
+
+   // cout << query(n, 1, n, b, c) <<'\n';
+    ll id; cin >> id;
+    if(id==1){
+      ll ind, val;
+      cin >> ind >> val;
+      ind++;
+      ///cout << ind <<' '<<val<<'\n';
+      update(1, 1, n, ind , val);
     }
+    else{
+      ll l,r; cin >> l >> r; 
+      l++;
+   //   cout << l << ' '<< r <<'\n';
+      cout << query(1, 1, n, l, r) <<'\n';
+    }
+   }
+
+
+  }
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  ll t =1; //cin >> t;
+  while (t--) {
+    solve();
+  }
 }
